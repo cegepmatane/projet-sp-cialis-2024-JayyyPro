7d8c27ac769404a2c1c6f332cdfe80a9558255e9
Julianoo <julien.quelin33@gmail.com>
2024-02-21 12:44:20 -0500

    Gestion du code

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { seededRandom } from 'three/src/math/MathUtils';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,7 +8,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// Load the glb file sol
 const loader = new GLTFLoader();
 
 const solPosition = [];
@@ -38,7 +36,6 @@ loader.load('POC_bureau.glb', function(gltf) {
 const crayonPosition = [];
 loader.load('POC_crayon.glb', function(gltf) {
     var model = gltf.scene;
-    // Positionner le crayon sur le bureau en utilisant ses dimensions et sa position
     model.position.set(
         bureauPosition[0].x + (- bureauDimensions[0].x + Math.random() * (bureauDimensions[0].x * 2)), 
         bureauDimensions[0].y * 2 + 0.4, 
@@ -47,17 +44,14 @@ loader.load('POC_crayon.glb', function(gltf) {
     scene.add(model);
 });
 
-// Set the camera position
 camera.position.z = 10;
 camera.position.y = 5;
 camera.position.x = 0;
 camera.lookAt(0, 0, 0);
 
-// Create light
 const light = new THREE.AmbientLight( 0xffffff, 0.5 );
 scene.add( light );
 
-// Animate the scene
 const animate = function () {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
