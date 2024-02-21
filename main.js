c53b645739458e45674155b98c212652dcc6a8c7
Julianoo <julien.quelin33@gmail.com>
2024-02-21 09:06:00 -0500

    Ajout objet crayon #2

--- main.js
+++ main.js
@@ -18,19 +18,17 @@ loader.load('POC_sol.glb', function(gltf) {
     scene.add(model);
 });
 
-var modelsToLoad = ['POC_bureau.glb'];
-modelsToLoad.forEach(function(modelFile) {
-    loader.load(modelFile, function(gltf) {
-        var model = gltf.scene;
-        model.position.set(Math.random() * 2.5, 0, Math.random() * 2.5);
-        scene.add(model);
-    });
+loader.load('POC_bureau.glb', function(gltf) {
+    var model = gltf.scene;
+    model.position.set(Math.random() * 5, 0, Math.random() * 5);
+    bureauPosition.push(model.position);
+    scene.add(model);
 });
 
 // Set the camera position
 camera.position.z = 10;
-camera.position.y = 5;
-camera.position.x = 0;
+camera.position.y = 7.5;
+camera.position.x = -2.5;
 camera.lookAt(0, 0, 0);
 
 // Create light
